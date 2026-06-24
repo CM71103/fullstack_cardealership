f2af6c873fa31a794a765f5bb690b976a3697ebd
CM71103 <chandramouli.allamaraju@gmail.com>
2026-06-24 08:08:57 -0400

    updated carModels

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,25 @@
-# from django.contrib import admin
-# from .models import related models
-
-
-# Register your models here.
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1
+
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'types', 'year')
+    list_filter = ('types', 'year')
+    search_fields = ('name',)
+
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    inlines = [CarModelInline]
+
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -147,8 +147,8 @@ STATICFILES_DIRS = [
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'chandramou34-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/',
+    'https://chandramou34-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/',
 ]
 CSRF_TRUSTED_ORIGINS = [
-    "https://chandramou34-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai"
+    "https://chandramou34-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
 ]
